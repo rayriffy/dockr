99887122ba779b4232a574683f25bd3b252fd765
Phumrapee Limpianchop <rayriffy@gmail.com>
2018-03-09 14:52:43 +0700

    Minor fix

--- dashboard/conmanage/index.php
+++ dashboard/conmanage/index.php
@@ -320,10 +320,10 @@
       Materialize.updateTextFields();
     });
     $('#addinput').click(function(){
-      $('#portinput').append("<div class='col l6 s12'><div class='input-field col s6'><input id='port_container' name='conport[]' type='number' maxlength='5' class='validate'><label for='port_container'>Container Port</label></div><div class='input-field col s6'><input id='port_bind' name='bindport[]' type='number' maxlength='5' class='validate'><label for='port_bind'>Foward to...</label></div></div>")
+      $('#portinput').append("<div class='col l6 s12'><div class='input-field col s6'><input id='port_container' name='conport[]' type='number' min='1024' max='49151' class='validate'><label for='port_container'>Container Port</label></div><div class='input-field col s6'><input id='port_bind' name='bindport[]' type='number' min='1024' max='49151' class='validate'><label for='port_bind'>Foward to...</label></div></div>")
     });
     $('#addvariable').click(function(){
-      $('#envinput').append("<div class='col l6 s12'><div class='input-field col s6'><input id='env_name' name='env_name[]' type='number' maxlength='5' class='validate'><label for='env_name'>Variable name</label></div><div class='input-field col s6'><input id='env_var' name='env_var[]' type='number' maxlength='5' class='validate'><label for='env_var'>Value</label></div></div>")
+      $('#envinput').append("<div class='col l6 s12'><div class='input-field col s6'><input id='env_name' name='env_name[]' type='text' class='validate'><label for='env_name'>Variable name</label></div><div class='input-field col s6'><input id='env_var' name='env_var[]' type='text' class='validate'><label for='env_var'>Value</label></div></div>")
     });
   </script>
 </body>
